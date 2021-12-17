98c61a0072584079f5781bd0b9cc93405fa93882
Chris Foster <chris.foster@thetrainline.com>
2021-12-17 17:56:13 +0000

    add some examples of filters

--- LuceneDemo/DirectoryBuilder.cs
+++ LuceneDemo/DirectoryBuilder.cs
@@ -1,5 +1,8 @@
 ﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Core;
+using Lucene.Net.Analysis.En;
 using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Analysis.Util;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
 using Lucene.Net.Store;
@@ -13,7 +16,7 @@ public class DirectoryBuilder : IDirectoryBuilder
 
     public DirectoryBuilder()
     {
-        _analyzer = new StandardAnalyzer(_luceneVersion);
+        _analyzer = new OurAnalyzer();
     }
 
     public Analyzer GetAnalyzer{ get => _analyzer; }
@@ -28,13 +31,85 @@ public class DirectoryBuilder : IDirectoryBuilder
         IndexWriterConfig config = new IndexWriterConfig(_luceneVersion, _analyzer);
         using IndexWriter iwriter = new IndexWriter(directory, config);
 
-        Document doc = new Document();
-        var text = "melons potatoes";
-        doc.Add(new Field("ingredients", text, TextField.TYPE_NOT_STORED));
-        doc.Add(new Field("Title", "potato stew", TextField.TYPE_STORED));
-        iwriter.AddDocument(doc);
+        var doc = CreateStandardDocument();
+        var doc2 = CreateCapitalizedCaseDocument();
+        var doc3 = CreatePluralDocument();
+        var doc4 = CreateFoldingDocument();
+
+
+
+        iwriter.AddDocument(doc, _analyzer);
+        iwriter.AddDocument(doc2, _analyzer);
+        iwriter.AddDocument(doc3, _analyzer);
+        iwriter.AddDocument(doc4, _analyzer);
         iwriter.Dispose();
 
         return directory;
+    }
+
+    private Document CreateStandardDocument()
+    {
+        Document doc = new Document();
+
+        //Standard example of getting title
+        doc.Add(new Field("Title", "potato stew", TextField.TYPE_STORED));
+
+        //Standard example to show normalisation
+        return doc;
+    }
+
+    private Document CreateCapitalizedCaseDocument()
+    {
+        Document doc = new Document();
+
+        //Standard example of getting title
+        doc.Add(new Field("Title", "Mash Potato", TextField.TYPE_STORED));
+
+        //Standard example to show normalisation
+        return doc;
+    }
+
+    private Document CreatePluralDocument()
+    {
+        Document doc = new Document();
+
+        //Standard example of getting title
+        doc.Add(new Field("Title", "Mash Potatoes", TextField.TYPE_STORED));
+
+        //Standard example to show normalisation
+        return doc;
+    }
+
+    private Document CreateFoldingDocument()
+    {
+        Document doc = new Document();
+
+        //Standard example of getting title
+        doc.Add(new Field("Title", "purée de pommes de terre", TextField.TYPE_STORED));
+
+        //Standard example to show normalisation
+        return doc;
+    }
+
+
+    
+}
+
+/// <summary>
+/// Analyser is responsible for buildng a token stream
+/// </summary>
+public class OurAnalyzer : StopwordAnalyzerBase
+{
+    //Analzer
+    //public OurAnalyzer() : base(LuceneVersion.LUCENE_48) {}
+
+    //This one has a stop words set that can remove common words
+    public OurAnalyzer() : base(LuceneVersion.LUCENE_48, StandardAnalyzer.STOP_WORDS_SET) { }
+    protected override TokenStreamComponents CreateComponents(string fieldName, TextReader reader)
+    {
+        var tokeniser = new StandardTokenizer(LuceneVersion.LUCENE_48, reader);
+        var lcfilter = new LowerCaseFilter(LuceneVersion.LUCENE_48, tokeniser);
+        var stemmer = new PorterStemFilter(lcfilter);
+        return new TokenStreamComponents(tokeniser, stemmer);
     }
 }
